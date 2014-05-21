89ca00e6481b1ec7227440f603674120e23afb9e
m_messiah <m.muzafarov@gmail.com>
2014-05-21 13:23:48 +0600

    Doctstrings and improved url handle

--- IISRewriterConv.py
+++ IISRewriterConv.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+    Script for converting Intelligencia.rewriter rules (ASPNet urlrewriter)
+    to IIS URL Rewrite 2.0 rules
+"""
 __author__ = 'm_messiah'
 
 try:
@@ -10,6 +14,10 @@ from re import sub, search
 
 
 class IISRewrite(object):
+    """
+        Class for converting Intelligencia.rewriter rules (ASPNet urlrewriter)
+        to IIS URL Rewrite 2.0 rules
+    """
     def __init__(self):
         tree = etree.parse(stdin)
         root = tree.getroot()
@@ -20,6 +28,9 @@ class IISRewrite(object):
         self.write()
 
     def create_rule(self, name, attr):
+        """
+            Convert single rule
+        """
         new_rule = etree.SubElement(self.new_rules, "rule",
                                     attrib={"name": attr["url"],
                                             "stopProcessing": "true"})
@@ -30,8 +41,9 @@ class IISRewrite(object):
         else:
             url = attr["url"]
         etree.SubElement(new_rule, "match",
-                         attrib={"url": attr["url"][0] + attr["url"][2:]
-                                 if attr["url"][1] == "/" else attr["url"]})
+                         attrib={"url": url[0] + url[2:]
+                                        if url[1] == "/"
+                                        else url})
         if param:
             condition = etree.SubElement(new_rule, "conditions")
             etree.SubElement(condition, "add",
@@ -39,13 +51,16 @@ class IISRewrite(object):
                                      "pattern": param})
         attributes = {"type": name.capitalize(),
                       "url": sub(r"\$(\d)",
-                                 "{C:\g<1>}" if param else "{R:\g<1>}",
+                                 r"{C:\g<1>}" if param else r"{R:\g<1>}",
                                  attr["to"])}
         if param:
             attributes["appendQueryString"] = "false"
         etree.SubElement(new_rule, "action", attrib=attributes)
 
     def convert(self):
+        """
+            Convert all rules from source
+        """
         for name, attr, rule in self.rules:
             if name in ("rewrite", "redirect"):
                 self.create_rule(name, attr)
@@ -55,6 +70,9 @@ class IISRewrite(object):
                 )
 
     def write(self):
+        """
+            Print converted rules
+        """
         print(etree.tounicode(self.new_rules, pretty_print=True))
         print("<!-- Failed convert. Please, handle it manually-->\n")
         print("\n".join(self.fails))
@@ -66,4 +84,4 @@ if __name__ == "__main__":
               "standard IIS >7 Rewriter.\n"
               "Conditions and other rules must be handled manually.")
         exit(1)
-    IISRewriter = IISRewrite()
+    IISRewrite()
